caa37d1f1aef309f1787ff61da85e7da72a71414
sonnyk07 <sonnyk07@gmail.com>
2021-05-16 14:33:42 +0300

    added variable lesson

---
+++ l04/variables.php
@@ -0,0 +1,15 @@
+<?php
+$fruit = 'apple';
+$fruit = 'banana';
+echo $fruit,'<br>';
+$fruit = 1000;
+
+
+//$1test = 1;(Переменная не может начинатся с цифры)
+$te2st = 1;//переменная может содержать цифру в любом месте только после буквы)
+//$%test = 1;//(Переменая не может начинаться с спец символа рим #,%,&,+,/,*,= и тд)
+$_test_rrr = 123;
+$my_lilttle_pony = 'my little ponny'; //( Переменная в стиле snakecase)
+$myLittlePony = 'MyLittlePony'; //(Переменная напиаана в ситле CamelCase)
+
+echo $my_lilttle_pony,'<br>',$myLittlePony;
