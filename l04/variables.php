7d5e6c5f94c1093dc4e2a060c45d506e8ca5beac
sonnyk07 <sonnyk07@gmail.com>
2021-05-16 20:04:40 +0300

    added finis lesson 05

--- l04/variables.php
+++ l04/variables.php
@@ -1,15 +1,15 @@
 <?php
-$fruit = 'apple';
-$fruit = 'banana';
-echo $fruit,'<br>';
-$fruit = 1000;
+    $fruit = 'apple';
+    $fruit = 'banana';
+    echo $fruit,'<br>';
+    $fruit = 1000;
 
 
 //$1test = 1;//Переменная не может начинатся с цифры//
-$te2st = 1;//переменная может содержать цифру в любом месте, но только после буквы//
+    $te2st = 1;//переменная может содержать цифру в любом месте, но только после буквы//
 //$%test = 1;//Переменая не может начинаться с спец-символа прим: #,%,&,+,/,*,=. и т.д//
-$_test_rrr = 123;
-$my_lilttle_pony = 'my little ponny'; //Переменная написано в стиле SnakeСase//
-$myLittlePony = 'MyLittlePony'; //Переменная написано в стиле CamelCase//
+    $_test_rrr = 123;
+    $my_lilttle_pony = 'my little ponny'; //Переменная написано в стиле SnakeСase//
+    $myLittlePony = 'MyLittlePony'; //Переменная написано в стиле CamelCase//
 
 echo $my_lilttle_pony,'<br>',$myLittlePony;
