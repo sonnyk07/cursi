0df8e8dfce5242cfa29b620e2eba0cbeb20b7681
sonnyk07 <83429187+sonnyk07@users.noreply.github.com>
2021-06-20 20:24:26 +0300

    Get sity works

--- l11/comment_processor.php
+++ l11/comment_processor.php
@@ -3,7 +3,7 @@ $comment = serialize($_POST);
 
 $error = worldFilter($_POST['comment']);
 if ($error){
-    header("location: error.php?massage={$error}");
+    header("Location: error.php?massage={$error}");
     exit;
 }
 
@@ -14,12 +14,12 @@ if (!is_dir($dir)){ //зодания файла//
 $file = time().'_'.md5($comment).'.log';//шифровка файла//
 $rout = "{$dir}/{$file}";//обеденям данные двух пернменных в роут//
 if (file_exists($rout)){//проверка если фал существует//
-    header('location: forms.php');//переаодресация скрипта//
+    header('Location: forms.php');//переаодресация скрипта//
     exit;//конец выполения программы//
 }
 
     file_put_contents($rout, $comment);//создания путя и фалы с данными в переменных//
-    header('location: forms.php');//переаодресация скрипта//
+    header('Location: forms.php');//переаодресация скрипта//
 
 function worldFilter(string $message): ? string
 {
